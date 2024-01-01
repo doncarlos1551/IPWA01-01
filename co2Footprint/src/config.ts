c89d7d850417b8f1d580c59de20f248cb516e8a9
Carl Ahrens <carl.ahrens@hotmail.de>
2024-01-01 19:36:34 +0100

    Es wurde "yarn run quasar build" durchgeführt und der inhalt des dist spa Ordners in das root Verzeichnis des gh-pages branches kopiert

--- co2Footprint/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const aktuellesJahr = new Date().getFullYear();
-export const firmenInformationen = {
-    firmenName: 'CO2FootPrint Fantasie GmbH', // Dieser Firmenname ist natürlich ausgedacht!
-    firmenEmail: 'info@co2-footprint-fantasie.com'
-}
-
