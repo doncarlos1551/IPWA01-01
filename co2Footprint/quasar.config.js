111607790c5a422ca76fe8a5b5464848d58ae49d
Carl Ahrens <carl.ahrens@hotmail.de>
2024-01-01 19:45:52 +0100

    Es wurde der public path im build abschnitt in der quasar config angepasst

--- co2Footprint/quasar.config.js
+++ co2Footprint/quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/IPWA01-01/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
